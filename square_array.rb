066b4a377f6d079e035250047dc25fb4d1cf3f30
Devin Wilson <craigvin84@yahoo.com>
2020-10-06 20:11:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,10 @@
 #def square_array(array)
-  new_array = []
-  array.each{|num| new_array.push(num**2)}
-  return new_array
-end
+#  new_array = []
+#  array.each{|num| new_array.push(num**2)}
+#  return new_array
+#end
 
 
 def square_array(array)
   array.each{|a|*a}
-  return new_array
 end
