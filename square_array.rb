c52b82ecbffb1355d3745dd972b11993eb31774c
Devin Wilson <craigvin84@yahoo.com>
2020-10-06 20:04:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
-#def square_array(array)
-#  array.each do {|num| ** 2}
-#end
-
-
 def square_array(array)
+  array.each {|num| ** 2}
+end
+
+
+#def square_array(array)
   new_array = []
   array.each{|a| new_array.push(a*a)}
   return new_array
